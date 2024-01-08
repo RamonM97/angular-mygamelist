77e1533426efe389b7f5fdb1ae915937e7f53f3d
RamonM97 <84131829+RamonM97@users.noreply.github.com>
2024-01-08 23:58:22 +0100

    Update environments.ts

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,7 @@
 
 export const environments = {
     urlAPI: 'https://api.rawg.io/api',
-    keyAPI: '?key=2be5e707a99b4b13baa6602e3d4c29c9',
+    keyAPI: '',
 
-    baseURL: 'https://angular-nest-auth-backend-production-01ad.up.railway.app'
+    baseURL: ''
 }
